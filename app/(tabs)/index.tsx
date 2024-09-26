578fe54a96e8e8c040dda7db1e9b720e5c43eeb0
siddhartha0 <siddhartha.sunuwar@gmail.com>
2024-09-26 18:35:41 +0545

    init

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,47 @@
+import { Image, StyleSheet, Platform, View, Text } from "react-native";
+
+import { HelloWave } from "@/components/HelloWave";
+import ParallaxScrollView from "@/components/ParallaxScrollView";
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
+
+export default function HomeScreen() {
+  return (
+    <ParallaxScrollView
+      headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
+      headerImage={
+        <Image
+          source={require("@/assets/images/partial-react-logo.png")}
+          style={styles.reactLogo}
+        />
+      }
+    >
+      <View style={styles.styleContainer}>
+        <Text>Hello</Text>
+        <ThemedView>
+          <ThemedText>Is this new ??</ThemedText>
+        </ThemedView>
+      </View>
+    </ParallaxScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  styleContainer: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 15,
+  },
+  stepContainer: {
+    gap: 8,
+    marginBottom: 8,
+  },
+  reactLogo: {
+    height: 178,
+    width: 290,
+    bottom: 0,
+    left: 0,
+    position: "absolute",
+  },
+});
